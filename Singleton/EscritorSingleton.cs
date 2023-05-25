af47e7e25a7100461db6fd63dcd3b5b967ead4de
jpgalvezr <134565107+jpgalvezr@users.noreply.github.com>
2023-05-24 20:35:43 -0500

    adicionar singleton
    adicionar archivo con desktop

---
+++ Singleton/EscritorSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Singleton
+{
+    internal class EscritorSingleton
+    {
+        private static EscritorSingleton instancia;
+        private static readonly object bloqueo = new object();
+
+        private EscritorSingleton() { 
+        }
+
+        public static EscritorSingleton Instancia { 
+            get
+            {
+                if (instancia == null)
+                {
+                    lock(bloqueo)
+                    {
+                        if (instancia == null) { 
+                            instancia = new EscritorSingleton();
+                        }
+                    }
+                }
+                return instancia;
+            }
+        }
+
+        public void EscribirMensaje(string mensaje) {
+            System.IO.StreamWriter arc = new StreamWriter(@"c:\temp\singleton.txt", true);
+            arc.WriteLine(DateTime.Now.ToShortTimeString() + " : " + mensaje.ToUpper());
+            arc.Close();
+            return;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string asteriscos = "*";
+            EscritorSingleton s1 = EscritorSingleton.Instancia;
+            EscritorSingleton s2 = EscritorSingleton.Instancia;
+            EscritorSingleton s3 = EscritorSingleton.Instancia;
+
+            s1.EscribirMensaje("Usando el S1, hola mundo ");
+            s2.EscribirMensaje("Usando el S2, hola mundo ");
+            s3.EscribirMensaje("Usando el S3, hola mundo ");
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                asteriscos += "*";
+                s1.EscribirMensaje("Usando el S1, hola mundo " + asteriscos);
+                for (int j = 0; j <= 10; j++)
+                {
+                    s2.EscribirMensaje("Usando el S2, hola mundo ");
+                    for (int k = 0; k <= 10; k++)
+                    {
+                        asteriscos += "*";
+                        s3.EscribirMensaje("Usando el S3, hola mundo " + asteriscos);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
